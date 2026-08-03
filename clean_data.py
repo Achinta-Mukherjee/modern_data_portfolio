055d19db2180a44920fb4a0c34d32ddb65ce003c
Achinta-Mukherjee <ecachinta4@gmail.com>
2026-08-04 00:35:50 +0530

    Added month-based revenue analysis

--- clean_data.py
+++ clean_data.py
@@ -14,14 +14,14 @@ df.columns = df.columns.str.replace(' ', '_').str.lower()
 
 
 # Objective 3: Data Inspection: Print the first 5 rows and the data types of the columns (df.info()) to ensure it loaded correctly.
-print("First 5 rows of the dataset:\n", df.head())
-print("\nData types of the columns:\n", df.info())
+# print("First 5 rows of the dataset:\n", df.head())
+# print("\nData types of the columns:\n", df.info())
 
 
 
 # Objective 4: Handle Missing Values: Check for any missing values in the dataset. If there are any, decide on a strategy to handle them (e.g., fill with mean/median, drop rows, etc.)
 missing_values = df.isnull().sum()
-print("Missing values in each column:\n", missing_values)
+# print("Missing values in each column:\n", missing_values)
 
 
 
@@ -33,22 +33,22 @@ df['date'] = pd.to_datetime(df['date'], errors='coerce')
 
 # Objective 6: Remove Duplicates: Check for any duplicate rows in the dataset and remove them if necessary.
 duplicates = df.duplicated().sum()
-print("Number of duplicate rows:", duplicates)
+# print("Number of duplicate rows:", duplicates)
 df = df.drop_duplicates()       
 
 
 
-# Objective 7: Group and Aggregate: We want to know which product category makes the most money. Create a new DataFrame called category_sales. Group the data by product_category and calculate the sum() of the total_amount
-category_sales = df.groupby('product_category')['total_amount'].sum().reset_index()
-print("Total sales by product category:\n", category_sales)
+# Objective 7: Create a brand new column in your DataFrame called month.
+df['month'] = df['date'].dt.month_name()
 
 
 
-#Objective 8: Sort the Data: Sort category_sales from highest revenue to lowest.
-category_sales = category_sales.sort_values(by='total_amount', ascending=False).reset_index(drop=True)
-print("Total sales by product category (sorted):\n", category_sales)    
+# Objective 8: Find out which month had the highest sales
+top_selling_months = df.groupby('month')['total_amount'].sum().reset_index()
+# print("Month with the highest sales:\n", top_selling_months)
 
 
 
-# Objective 9: Save your summarized category_sales DataFrame to a new file called top_selling_categories.csv
-category_sales.to_csv('top_selling_categories.csv')
+#Objective 8: Sort the Data on total_amount and export the file as top_selling_months.csv
+top_selling_months.sort_values(by='total_amount', ascending=False, inplace=True)
+top_selling_months.to_csv('top_selling_months.csv')
